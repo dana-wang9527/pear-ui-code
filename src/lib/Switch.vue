8d2af66355578411dfa69f456176e9819f7a875d
wangdana <wangdana1998@gmail.com>
2021-04-02 15:47:35 +0800

    hover之后可以右滚

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,12 +1,12 @@
 <template>
-<button><span></span></button>
+  <button><span></span></button>
 </template>
 
 
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
+button {
   height: $h;
   width: $h*2;
   border: none;
@@ -14,13 +14,18 @@ button{
   border-radius: $h/2;
   position: relative;
 }
-span{
+
+span {
   position: absolute;
   top: 2px;
   left: 2px;
   height: $h2;
   width: $h2;
-  background:white;
+  background: white;
   border-radius: $h2 / 2;
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px)
 }
 </style>
