598f304cc9a94e6e1d798cfac9a2d1cffa9ecc97
wangdana <wangdana1998@gmail.com>
2021-04-02 21:41:54 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,14 +3,25 @@
     button文档
   </div>
   <h1>示例</h1>
-  <Button>你好,</Button>
+  <Button @click="onClick"
+          @focus="onClick"
+          @mouseover="onClick"
+  >你好</Button>
+
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
-export  default {
-  components:{Button}
-}
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    };
+    return {onClick};
+  }
+};
 </script>
 
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,17 +1,28 @@
 <template>
-  <button>
-    <slot/>
-我是按钮
+  <div :size="size">
+  <button v-bind="rest">
+ <slot/>
+<!--    插槽，用户传进来的&ndash;-->
   </button>
+  </div>
 </template>
 
 <script lang="ts">
 
 export default  {
+  inheritAttrs:false,
+  props:{
+  },
+  setup(props,context){
+    const {size,...rest}=context.attrs
+    return {size,rest}
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
-
+div{
+  border: 1px solid red;
+}
 </style>
