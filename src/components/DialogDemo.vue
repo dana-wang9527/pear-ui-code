7e4aa0362501539681dcfbdd1b4ffc6811bc2197
wangdana <wangdana1998@gmail.com>
2021-04-03 13:51:06 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,8 +1,13 @@
 <template>
   <div>Dialog文档</div>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components:{Dialog}
+}
 
 </script>
 
