a82c058606184e7b0a184f6dd646a13e4990a953
wangdana <wangdana1998@gmail.com>
2021-04-03 15:12:22 +0800

    Dialog支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,20 @@
 <template>
   <template v-if="visible">
-  <div class="pear-dialog-overlay"></div>
-  <div class="pear-dialog-wrapper">
-    <div class="pear-dialog">
-      <header>标题 <span class="pear-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">Ok</Button>
-        <Button>Cancel</Button>
-      </footer>
+    <div class="pear-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="pear-dialog-wrapper">
+      <div class="pear-dialog">
+        <header>标题
+          <span @click="close" class="pear-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">Ok</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
   </template>
 </template>
 
@@ -22,13 +23,43 @@
 import Button from './Button.vue';
 
 export default {
-  props:{
-    visible:{
-      type:Boolean,
-      default:false
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function
+    },
+    cancel: {
+      type: Function
     }
   },
-  components: {Button}
+  components: {Button},
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok && props.ok() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    };
+    return {close, onClickOverlay, ok, cancel};
+  }
 };
 </script>
 
@@ -41,6 +72,7 @@ $border-color: #d9d9d9;
   box-shadow: 0 0 3px fade_out(black, 0.5);
   min-width: 18em;
   max-width: 90%;
+
   &-overlay {
     position: fixed;
     top: 0;
@@ -50,6 +82,7 @@ $border-color: #d9d9d9;
     background: fade_out(black, 0.5);
     z-index: 10;
   }
+
   &-wrapper {
     position: fixed;
     left: 50%;
@@ -57,7 +90,8 @@ $border-color: #d9d9d9;
     transform: translate(-50%, -50%);
     z-index: 11;
   }
-  >header {
+
+  > header {
     padding: 12px 16px;
     border-bottom: 1px solid $border-color;
     display: flex;
@@ -65,20 +99,24 @@ $border-color: #d9d9d9;
     justify-content: space-between;
     font-size: 20px;
   }
-  >main {
+
+  > main {
     padding: 12px 16px;
   }
-  >footer {
+
+  > footer {
     border-top: 1px solid $border-color;
     padding: 12px 16px;
     text-align: right;
   }
+
   &-close {
     position: relative;
     display: inline-block;
     width: 16px;
     height: 16px;
     cursor: pointer;
+
     &::before,
     &::after {
       content: '';
@@ -89,9 +127,11 @@ $border-color: #d9d9d9;
       top: 50%;
       left: 50%;
     }
+
     &::before {
       transform: translate(-50%, -50%) rotate(-45deg);
     }
+
     &::after {
       transform: translate(-50%, -50%) rotate(45deg);
     }
