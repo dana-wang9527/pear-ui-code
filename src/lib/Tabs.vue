6030c5f1393e89389281da0eb6a283a9b94350d6
wangdana <wangdana1998@gmail.com>
2021-04-03 22:35:39 +0800

    添加Tabs组件样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,24 +1,61 @@
 <template>
-  <div>
-  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="pear-tabs">
+    <div class="pear-tabs-nav">
+      <div class="pear-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="pear-tabs-content">
+      <component class="pear-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
 
+
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from './Tab.vue';
+
 export default {
-  setup(props,context){
-    const defaults=context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type!==Tab){
-        throw new Error('Tabs 子标签必须是 Tab')
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab');
       }
-    })
-   const titles= defaults.map((tag)=>{
-      return tag.props.title
-    })
-    return{defaults,titles}
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {defaults, titles};
+  }
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.pear-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
   }
 }
-</script>
+</style>
