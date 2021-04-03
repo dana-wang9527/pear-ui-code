b20a62cf4548d30c76ce1a71b15d384d39ce9f56
wangdana <wangdana1998@gmail.com>
2021-04-03 11:40:15 +0800

    添加了level和disabled属性和相关按钮

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="pear-button" :class="classes">
+  <button class="pear-button"
+          :class="classes"
+          :disabled="disable">
+    <span class="pear-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -16,14 +19,23 @@ export default {
     size: {
       type: String,
       default: 'normal'
-    }
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
+    disable: {
+      type: Boolean,
+      default: false
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`pear-theme-${theme}`]: theme,
         [`pear-size-${size}`]: size,
+        [`pear-level-${level}`]: level
       };
     });
     return {classes};
@@ -39,6 +51,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .pear-button {
   box-sizing: border-box;
   height: $h;
@@ -53,6 +67,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -103,6 +118,81 @@ $radius: 4px;
       font-size: 12px;
       height: 20px;
       padding: 0 4px;
+    }
+  }
+
+  &.pear-theme-button {
+    &.pear-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.pear-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.pear-theme-link {
+    &.pear-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.pear-theme-text {
+    &.pear-level-main {
+      color: $blue;
+
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.pear-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.pear-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+
+  &.pear-theme-link, &.pear-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
     }
   }
 }
