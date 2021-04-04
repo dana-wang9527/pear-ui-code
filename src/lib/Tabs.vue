023c226c1ef5e67faa13671b0cda34f820100b04
wangdana <wangdana1998@gmail.com>
2021-04-04 09:51:21 +0800

    加下划线和其样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,6 +4,7 @@
       <div class="pear-tabs-nav-item" @click="select(t)" :class="{selected:t===selected}" v-for="(t,index) in titles"
            :key="index">{{ t }}
       </div>
+      <div class="pear-tabs-nav-indicator"></div>
     </div>
     <div class="pear-tabs-content">
       <component class="pear-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c"/>
@@ -54,6 +55,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -67,6 +69,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
 
