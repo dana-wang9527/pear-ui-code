62114f79f4685699809b31493bb8cc77fd434d90
wangdana <wangdana1998@gmail.com>
2021-04-04 13:26:19 +0800

    给主页面添加背景色

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topNavAndBanner">
     <TopNav/>
     <div class="banner">
       <h1>梨子ui</h1>
@@ -23,13 +23,15 @@ export default {
 
 
 <style lang="scss" scoped>
+.topNavAndBanner{
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
   > .actions{
     padding: 8px 0;
     a {
