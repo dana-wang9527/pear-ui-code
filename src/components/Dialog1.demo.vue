cd8b81fc7612102d8aa76a207fe99f681bd3e83f
wangdana <wangdana1998@gmail.com>
2021-04-04 22:03:58 +0800

    改造Dialog Demo

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,46 @@
+<demo>
+常规使用
+</demo>
+
+<template>
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+      <template v-slot:content>
+        <div>你好</div>
+        <div>你好2</div>
+      </template>
+      <template v-slot:title>
+        <strong>提示</strong>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue'
+export default {
+  components: {
+    Dialog,
+    Button
+  },
+  setup() {
+    const x = ref(false)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {}
+    return {
+      x,
+      toggle,
+      f1,
+      f2
+    }
+  }
+}
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,53 +1,19 @@
 <template>
-  <div>Dialog文档</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-      <div>你好</div>
-      <div>你好2</div>
-    </template>
-    <template v-slot:title>
-      <strong>提示</strong>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-  <Button @click="showDialog">show</Button>
+  <div>Dialog示例</div>
+  <Demo :component="Dialog1Demo"/>
+  <Demo :component="Dialog2Demo"/>
 </template>
 
+
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
-import {openDialog} from '../lib/openDialog'
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1.demo.vue';
+import Dialog2Demo from './Dialog2.demo.vue';
 
 export default {
-  components: {Dialog, Button},
+  components: {Demo},
   setup() {
-    const x = ref(false);
-    const toggle = () => {
-      x.value = !x.value;
-    };
-    const f1 = () => {
-      return false;
-    };
-    const f2 = () => {};
-    const showDialog = () => {
-      openDialog({
-        title: '标题',
-        content: '你好',
-        ok(){
-          return false
-        },
-        cancel(){
-          console.log('cancel');
-        },
-        closeOnClickOverlay(){
-          return false
-        }
-      });
-    };
-    return {x, toggle, f1, f2, showDialog};
+    return {Dialog1Demo, Dialog2Demo};
   },
 };
 </script>
